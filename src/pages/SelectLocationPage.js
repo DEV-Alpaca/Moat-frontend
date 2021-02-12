c54308ba30f784038837a7b7af4f5184b96c3dea
simyeen <simyeen@naver.com>
2021-02-12 16:04:26 +0900

    feat : pages/selectLacation

--- src/pages/SelectLocationPage.js
+++ src/pages/SelectLocationPage.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Header from '../components/common/Header';
+import Nav from '../components/common/Nav';
+import SelectLocation from '../components/write/SelectLocation';
 
 const SelectLocationPage = () => {
-  return <div>모임선택페이지</div>;
+  return (
+    <>
+      <Nav close text={'모임 만들기'} />
+      <SelectLocation />
+    </>
+  );
 };
 
 export default SelectLocationPage;
