84f963df673d5d370c780174a6427e50a34d3b8c
simyeen <simyeen@naver.com>
2021-03-03 16:39:25 +0900

    fix/overflow

--- src/components/write/WriteForm.js
+++ src/components/write/WriteForm.js
@@ -31,7 +31,6 @@ const CameraBlock = styled.label`
   border-radius: 8px;
   border: solid 1px ${palette.gray[100]};
   background-color: ${palette.gray[50]};
-  letter-spacing: -0.7px;
   color: #808080;
 
   cursor: pointer;
@@ -65,13 +64,14 @@ const StyledTextarea = styled.textarea`
 const ImgContainer = styled.div`
   display: flex;
   align-items: center;
-  label + img {
+  overflow: auto;
+  /* label + img {
     margin-left: ${r[16]}rem;
   }
 
   img + img {
     margin-left: ${r[16]}rem;
-  }
+  } */
 `;
 
 const Container = styled.div`
